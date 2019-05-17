29131a562c08b51fd21cec9f2aba717b2b5667a8
Przemysław Jurkiewicz <przemko.jurkiewicz@gmail.com>
2019-05-17 10:57:06 +0200

    Add Players and PlayerList Components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
 import React, { Component } from 'react';
 import './App.css';
+import PlayerList from './components/playerList/PlayerList';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      players: [
+        {
+          name: 'Kunegunda',
+          score: 5,
+        },
+        {
+          name: 'Antoś',
+          score: 0,
+        },
+        {
+          name: 'Marian',
+          score: 15,
+        },
+        {
+          name: 'Zosia',
+          score: 1,
+        }
+      ]
+    }
+  }
+
   render() {
     return (
-      <div className="App">
+      <div className='App'>
+        <PlayerList players={this.state.players} />
       </div>
     );
   }
+
 }
 
 export default App;
