b895e8ac30e2c164546f899cb7b590d0a6761533
Przemysław Jurkiewicz <przemko.jurkiewicz@gmail.com>
2019-05-17 09:12:27 +0200

    Add playerList component

---
+++ src/components/playerList/PlayerList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Player from '../player/Player';
+import './PlayerList.css';
+
+const PlayerList = (props) => (
+    <ul className='PlayerList'>
+        {props.players.map((player, i) => (
+            <Player
+                key={i}
+                name={player.name}
+                score={player.score}
+                onPlayerScoreChange={(points) => props.onScoreUpdate(i, points)}
+            />
+        ))}
+    </ul>
+);
+
+export default PlayerList;
---
+++ src/components/playerList/PlayerList.test.js
@@ -0,0 +1,48 @@
+import PlayerList from './PlayerList';
+import Player from '../player/Player';
+import React from 'react';
+import { shallow } from 'enzyme';
+
+it('renders without crashing', () => {
+    shallow(<PlayerList players={[]} />);
+});
+
+it('renders correct number of players', () => {
+    const players = [
+        {
+            name: 'Kunegunda',
+            score: 5
+        },
+        {
+            name: 'Antoś',
+            score: 0
+        }
+    ]
+    const playerComponent = shallow(<PlayerList players={players} />);
+    console.log(playerComponent.debug());
+
+    const expectedPlayersNumber = playerComponent.find(Player).length;
+    expect(expectedPlayersNumber).toEqual(2);
+});
+
+it('renders correct score update', () => {
+    const players = [
+        {
+            name: 'Kunegunda',
+            score: 5
+        },
+        {
+            name: 'Antoś',
+            score: 0
+        }
+    ]
+    const mockedOnScoreUpdate = jest.fn();
+
+    const playerComponent = shallow(<PlayerList players={players} onScoreUpdate={mockedOnScoreUpdate} />);
+    
+    const firstPlayer = playerComponent.find(Player).first();
+    const onPlayerScoreChange = firstPlayer.prop('onPlayerScoreChange');
+    onPlayerScoreChange(10);
+    
+    expect(mockedOnScoreUpdate).toBeCalledWith(0,10);
+});
