74a1edb8373e1c3f41125381db72051501d0cf94
Przemysław Jurkiewicz <przemko.jurkiewicz@gmail.com>
2019-05-17 11:28:45 +0200

    Add update player score test

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,27 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
+import PlayerList from './components/PlayerList/PlayerList'
 
 it('renders without crashing', () => {
   shallow(<App />);
+});
+
+it('should update player score', () => {
+  const players = [
+    {
+        name: 'Kunegunda',
+        score: 5
+    }
+  ]
+
+  const appComponent = shallow(<App />);
+  appComponent.setState({players});
+
+  const onScoreUpdate = appComponent.find(PlayerList).prop('onScoreUpdate');
+  onScoreUpdate(0, 5);
+  const playersAfterUpdate = appComponent.state().players;
+  
+  expect(playersAfterUpdate[0].score).toEqual(10);
+
 });
